f5635a089f57d177f532230336c9efa4670dd672
Constancegit status <constanceingram94@gmail.com>
2021-07-22 15:07:00 -0500

    added CS and resume

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { AnimateSharedLayout, AnimatePresence, motion } from "framer-motion"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimatePresence exitBeforeEnter
+        initial={{ opacity : 0 }}
+        animate={{ opacity: 1 }} 
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <Component {...pageProps} />
+    </AnimatePresence>
+    )
 }
 
 export default MyApp
+
